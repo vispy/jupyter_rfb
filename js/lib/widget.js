65a7753b10b61f92b08eb21e28c32e3532eb2b22
Almar Klein <almar@almarklein.org>
2021-06-30 15:39:01 +0200

    more progress

--- js/lib/widget.js
+++ js/lib/widget.js
@@ -21,13 +21,20 @@ var _ = require('lodash');
 // differ from the defaults will be specified.
 var RemoteFrameBufferModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
+        // Meta info
         _model_name : 'RemoteFrameBufferModel',
         _view_name : 'RemoteFrameBufferView',
         _model_module : 'jupyterfb',
         _view_module : 'jupyterfb',
         _model_module_version : '0.1.0',
         _view_module_version : '0.1.0',
-        value : 'Hello World!'
+        // For the frames
+        last_index : 0,
+        last_timestamp : 0.0,
+        // For the widget
+        css_width : '100%',
+        css_height : '300px',
+        resizable : true
     })
 });
 
@@ -36,18 +43,143 @@ var RemoteFrameBufferModel = widgets.DOMWidgetModel.extend({
 var RemoteFrameBufferView = widgets.DOMWidgetView.extend({
     // Defines how the widget gets rendered into the DOM
     render: function() {
-        this.value_changed();
+        var that = this;
 
-        // Observe changes in the value traitlet in Python, and define
-        // a custom callback.
-        this.model.on('change:value', this.value_changed, this);
+        // Create image element
+        this.img = document.createElement("img");
+        this.img.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAIAAAAlC+aJAAAAoklEQVR42u3PoQ3AQADDwN9/sm7V0qdVQGLJgUa581x7r1H6WTskoH3oN2DhRNIFtDsfsHAi6QLanQ9YOJF0Ae3OByycSLqAducDFk4kXUC78wELJ5IuoN35gIUTSRfQ7nzAwomkC2h3PmDhRNIFtDsfsHAi6QLanQ9YOJF0Ae3OByycSLqAducDFk4kXUC78wELJ5IuoN35gIUTSRfQ7njAB3//qnevFJyZAAAAAElFTkSuQmCC";
+        this.el.appendChild(this.img);
+        
+        // Receive custom messages (i.e. new image data)
+        this.model.on('msg:custom', this.on_msg, this);
+
+        // Initialize sizing. Setting the this.el's size right now has no effect for some reason, so we use a timer.
+        this.on_resizable();
+        this.img.style.width = "100%";
+        this.img.style.height = "100%"; 
+        window.setTimeout(() => { this.el.style.width = this.model.get('css_width'); }, 1);
+        window.setTimeout(() => { this.el.style.height = this.model.get('css_height'); }, 1);
+
+        // Keep track of size changes from the server        
+        this.model.on('change:css_width', function () { this.el.style.width = this.model.get('css_width'); }, this);
+        this.model.on('change:css_height', function () { this.el.style.height = this.model.get('css_height'); }, this);
+        this.model.on('change:resizable', this.on_resizable, this);
+
+        // Keep track of size changes in JS, so we can notify the server
+        this._current_size = [0, 0, 1];
+        this.el.addEventListener("resize", this.check_resize.bind(this));
+        window.addEventListener("resize", this.check_resize.bind(this));
+        window.setInterval(this.check_resize.bind(this), 500);
+
+        // Mouse events
+        this.el.addEventListener('mousedown', function (e) {
+            that.send(create_pointer_event(that.el, e, "mouse_down"));
+            e.preventDefault();
+        });
+        this.el.addEventListener('mouseup', function (e) {
+            that.send(create_pointer_event(that.el, e, "mouse_up"));
+            e.preventDefault();
+        });
+        // TODO: mouse move
+        // TODO: wheel event
+        this.el.addEventListener('touchstart', function (e) {
+            that.send(create_pointer_event(that.el, e, "touch_down"));
+        });
+        this.el.addEventListener('touchend', function (e) {
+            that.send(create_pointer_event(that.el, e, "touch_up"));
+        });
+        this.el.addEventListener('touchcancel', function (e) {
+            that.send(create_pointer_event(that.el, e, "touch_up"));
+        });
+        this.el.addEventListener('touchmove', function (e) {
+            that.send(create_pointer_event(that.el, e, "touch_move"));
+        });
+    },
+    
+    on_msg: function(msg, buffers) {
+        if (msg.type == "framebufferdata") {
+            // Update image (it's an in-line image)
+            this.img.src = msg.src;
+            // Let Python know what we have at the view
+            this.model.set('last_index', msg.index);
+            this.model.set('last_timestamp', msg.timestamp);
+            this.touch();
+        }
     },
 
-    value_changed: function() {
-        this.el.textContent = this.model.get('value');
-    }
+    on_resizable: function () {
+        this.el.style.resize = this.model.get('resizable') ? 'both' : 'none';
+    },
+
+    check_resize: function() {
+        // Called when the widget resizes. Width and height are in logical pixels.
+        let w = this.img.clientWidth;
+        let h = this.img.clientHeight;
+        let r = window.devicePixelRatio;
+        if (this._current_size[0] != w || this._current_size[1] != h || this._current_size[2] != r) {
+            this._current_size = [w, h, r];
+            this.send({event_type: 'resize', width: w, height: h, pixel_ratio: r});
+        }
+    },
+
+
 });
 
+
+function create_pointer_event(el, e, event_type) {
+    let rect = el.getBoundingClientRect();
+    let offset = [rect.left, rect.top];
+
+    if (event_type.startsWith("touch")) {
+        // Touch event - select one touch to represent the main position
+        let t = e.changedTouches[0];
+        var pos = [Number(t.clientX - offset[0]), Number(t.clientY - offset[1])];
+        var page_pos = [t.pageX, t.pageY];
+        var button = 0;
+        var buttons = [];
+        // Include basic support for multi-touch
+        var touches = {};
+        for (let i=0; i<e.changedTouches.length; i++) {
+            let t = e.changedTouches[i];
+            if (t.target !== e.target) { continue; }
+            touches[t.identifier] = [Number(t.clientX - offset[0]), Number(t.clientY - offset[1]), t.force];
+        }
+        var ntouches = e.touches.length;
+    } else {
+        // Mouse event
+        var pos = [Number(e.clientX - offset[0]), Number(e.clientY - offset[1])];
+        var page_pos = [e.pageX, e.pageY];
+        // Fix buttons
+        var buttons_mask = [];
+        if (e.buttons > 0) {
+            buttons_mask = Array.from(e.buttons.toString(2)).reverse();
+        } else if (e.which) {
+            buttons_mask = [e.which.toString(2)]  // e.g. Safari (but also 1 for RMB)
+        } else {
+            // libjavascriptcoregtk-3.0-0  version 2.4.11-1 does not define e.buttons
+            buttons_mask = [e.button.toString(2)]
+        }
+        var buttons = [1, 2, 3, 4, 5].filter((i) => buttons_mask[i-1] == "1");
+        var button = {0: 1, 1: 3, 2: 2, 3: 4, 4: 5}[e.button];
+        var touches = {'-1': [pos[0], pos[1], 1]};  // key must not clash with real touches
+        var ntouches = buttons.length;
+    }
+
+    // note: our button has a value as in JS "which"
+    modifiers = ["Alt", "Shift", "Ctrl", "Meta"].filter((n) => e[n.toLowerCase() + 'Key']);
+    
+    // Create event dict
+    return {
+        event_type: event_type,
+        pos: pos,
+        page_pos: page_pos,
+        touches: touches,
+        ntouches: ntouches,
+        button: button,
+        buttons: buttons,
+        modifiers: modifiers,
+    }
+}
 
 module.exports = {
     RemoteFrameBufferModel: RemoteFrameBufferModel,
--- jupyterfb/widget.py
+++ jupyterfb/widget.py
@@ -1,10 +1,59 @@
+import time
+from base64 import encodebytes
+
 import ipywidgets as widgets
-from traitlets import Unicode
+from traitlets import Unicode, Int, Float, Bool
+import tornado
+
+from ._png import array2png
+
 
 # See js/lib/example.js for the frontend counterpart to this file.
 
+
+class FrameSenderMixin:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._received_frames = 0
+        self._send_frames = 0
+        self._pending_frames = []
+
+        self._timer = tornado.ioloop.PeriodicCallback(self._iter, 20, 0.05)
+        self._timer.start()
+
+    def push_frame(self, array):
+        self._received_frames += 1
+        self._pending_frames.append(array)
+        self._iter()
+
+    def _iter(self, *args):
+        if self._pending_frames:
+            if self.last_index >= self._send_frames:
+                if self.last_timestamp + 0.5 < time.time():
+                    self._send_frame(self._pending_frames.pop(0))
+
+    def _send_frame(self, array):
+            timestamp = time.time()
+            self._send_frames += 1
+
+            # Turn array into a based64-encoded PNG
+            png_data = array2png(array)
+            preamble = "data:image/png;base64,"
+            src = preamble + encodebytes(png_data).decode()
+
+            # Compose message and send
+            msg = dict(
+                type="framebufferdata",
+                src=src,
+                index=self._send_frames,
+                timestamp=timestamp,
+            )
+            self.send(msg)
+
+
 @widgets.register
-class RemoteFrameBuffer(widgets.DOMWidget):
+class RemoteFrameBuffer(widgets.DOMWidget, FrameSenderMixin):
     """Widget that shows a remote frame buffer."""
 
     # Name of the widget view class in front-end
@@ -24,8 +73,28 @@ class RemoteFrameBuffer(widgets.DOMWidget):
     # Version of the front-end module containing widget model
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
 
-    # Widget specific property.
-    # Widget properties are defined as traitlets. Any property tagged with `sync=True`
-    # is automatically synced to the frontend *any* time it changes in Python.
-    # It is synced back to Python from the frontend *any* time the model is touched.
-    value = Unicode('Hello World!').tag(sync=True)
+    # Widget specific properties
+    last_index = Int(0).tag(sync=True)
+    last_timestamp = Float(time.time()).tag(sync=True)
+
+    css_width = Unicode("100%").tag(sync=True)
+    css_height = Unicode("300px").tag(sync=True)
+    resizable = Bool(True).tag(sync=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.on_msg(self._receive_events)
+
+    def _receive_events(self, widget, content, buffers):
+        print(content)
+        # if content['msg_type'] == 'init':
+        #     self.canvas_backend._reinit_widget()
+        # elif content['msg_type'] == 'events':
+        #     events = content['contents']
+        #     for ev in events:
+        #         self.gen_event(ev)
+        # elif content['msg_type'] == 'status':
+        #     if content['contents'] == 'removed':
+        #         # Stop all timers associated to the widget.
+        #         _stop_timers(self.canvas_backend._vispy_canvas)
+
