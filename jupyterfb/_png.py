d80bf3b19dba4d7c768156fa833d875e28d856a5
Almar Klein <almar@almarklein.org>
2021-06-30 12:06:01 +0200

    progress

---
+++ jupyterfb/_png.py
@@ -0,0 +1,78 @@
+import io
+import zlib
+import struct
+
+import numpy as np
+
+
+def array2png(array, file=None):
+    """
+    Create a png image from a numpy array. The written image is in RGB
+    or RGBA format, with 8 bit precision, zlib-compressed, without interlacing.
+
+    Parameters:
+        array (ndarray): the image data to write. The shape must be either
+        NxM (grayscale), NxMx1 (grayscale), NxMx3 (RGB) or NxNx4 (RGBA).
+    """
+
+    # Check types
+    if hasattr(array, 'shape') and hasattr(array, 'dtype'):
+        if array.dtype != 'uint8':
+            raise TypeError('Image array to convert to PNG must be uint8')
+        original_shape = shape = array.shape
+    else:
+        raise ValueError(f"Invalid type for array, need ndarray-like, got {type(array)}")
+
+    # Allow grayscale: convert to RGB
+    if len(shape) == 2 or (len(shape) == 3 and shape[2] == 1):
+        shape = shape[0], shape[1], 3
+        array = array.reshape(shape[:2])
+        array3 = np.empty(shape, np.uint8)
+        array3[..., 0] = array
+        array3[..., 1] = array
+        array3[..., 2] = array
+        array = array3
+
+    # Check shape
+    if not (len(shape) == 3 and shape[2] in (3, 4)):
+        raise ValueError(f'Unexpected image shape: {original_shape}')
+
+    # Get file object
+    f = io.BytesIO() if file is None else file
+
+    def add_chunk(data, name):
+        name = name.encode('ASCII')
+        crc = zlib.crc32(data, zlib.crc32(name))
+        f.write(struct.pack('>I', len(data)))
+        f.write(name)
+        f.write(data)
+        f.write(struct.pack('>I', crc & 0xffffffff))
+
+    # Write ...
+
+    # Header
+    f.write(b'\x89PNG\x0d\x0a\x1a\x0a')
+
+    # First chunk
+    w, h = shape[1], shape[0]
+    depth = 8
+    ctyp = 0b0110 if shape[2] == 4 else 0b0010
+    ihdr = struct.pack('>IIBBBBB', w, h, depth, ctyp, 0, 0, 0)
+    add_chunk(ihdr, 'IHDR')
+
+    # Chunk with pixels. Just one chunk, no fancy filters.
+    compressor = zlib.compressobj(level=7)
+    compressed_data = []
+    line_len = shape[1] * shape[2]
+    for row_index in range(shape[0]):
+        row = array[row_index]
+        compressed_data.append(compressor.compress(b'\x00'))  # prepend filter byter
+        compressed_data.append(compressor.compress(row))
+    compressed_data.append(compressor.flush())
+    add_chunk(b"".join(compressed_data), 'IDAT')
+
+    # Closing chunk
+    add_chunk(b'', 'IEND')
+
+    if file is None:
+        return f.getvalue()
---
+++ tests/test_png.py
@@ -0,0 +1,110 @@
+"""
+Test png module
+"""
+
+import os
+import sys
+import tempfile
+
+from jupyterfb._png import array2png
+import numpy as np
+from pytest import raises
+
+
+tempdir = tempfile.gettempdir()
+
+shape0 = 100, 100
+im0 = b'\x77' * 10000
+
+shape1 = 5, 5
+im1 = b''
+im1 += b'\x00\x00\x99\x00\x00'
+im1 += b'\x00\x00\xff\x00\x00'
+im1 += b'\x99\xff\xff\xff\x99'
+im1 += b'\x00\x00\xff\x00\x00'
+im1 += b'\x00\x00\x99\x00\x00'
+
+shape2 = 6, 6
+im2 = b''
+im2 += b'\x00\x00\x00\x88\x88\x88'
+im2 += b'\x00\x00\x00\x88\x88\x88'
+im2 += b'\x00\x00\x00\x88\x88\x88'
+im2 += b'\x44\x44\x44\xbb\xbb\xbb'
+im2 += b'\x44\x44\x44\xbb\xbb\xbb'
+im2 += b'\x44\x44\x44\xbb\xbb\xbb'
+
+shape3 = 5, 5, 3
+im3 = bytearray(5*5*3)
+im3[0::3] = im0[:25]
+im3[1::3] = im1[:25]
+im3[2::3] = im2[:25]
+
+shape4 = 5, 5, 4
+im4 = bytearray(5*5*4)
+im4[0::4] = im0[:25]
+im4[1::4] = im1[:25]
+im4[2::4] = im2[:25]
+im4[3::4] = im0[:25]
+
+im0 = np.frombuffer(im0, np.uint8).reshape(shape0)
+im1 = np.frombuffer(im1, np.uint8).reshape(shape1)
+im2 = np.frombuffer(im2, np.uint8).reshape(shape2)
+im3 = np.frombuffer(im3, np.uint8).reshape(shape3)
+im4 = np.frombuffer(im4, np.uint8).reshape(shape4)
+
+ims = im0, im1, im2, im3, im4
+shapes = shape0, shape1, shape2, shape3, shape4
+
+
+def test_writing():
+
+    # Get bytes
+    b0 = array2png(im0)
+    b1 = array2png(im1)
+    b2 = array2png(im2)
+    b3 = array2png(im3)
+    b4 = array2png(im4)
+
+    blobs = b0, b1, b2, b3, b4
+
+    # Write to disk (also for visual inspection)
+    for i in range(5):
+        filename = os.path.join(tempdir, 'test%i.png' % i)
+        with open(filename, 'wb') as f:
+            f.write(blobs[i])
+    print('wrote PNG test images to', tempdir)
+
+    assert len(b1) < len(b4)  # because all zeros are easier to compress
+
+    # Check that providing file object yields same result
+    with open(filename+'.check', 'wb') as f:
+        array2png(im4, f)
+    bb1 = open(filename, 'rb').read()
+    bb2 = open(filename+'.check', 'rb').read()
+    assert len(bb1) == len(bb2)
+    assert bb1 == bb2
+
+    # Test shape with singleton dim
+    b1_check = array2png(im1.reshape(shape1[0], shape1[1], 1))
+    assert b1_check == b1
+
+
+def test_writing_failures():
+
+    with raises(ValueError):
+        array2png([1, 2, 3, 4])
+
+    with raises(ValueError):
+        array2png(b'x'*10)
+
+    with raises(ValueError):
+        array2png(im0.reshape(-1))
+
+    with raises(ValueError):
+        array2png(im4.reshape(-1, -1, 8))
+        assert (im_check[:,:,i] == im).all()
+
+
+if __name__ == "__main__":
+    test_writing()
+    test_writing_failures()
