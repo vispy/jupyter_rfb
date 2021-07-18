aa4b5b33c31fc9e59230607391e01fc613ecc41f
Almar Klein <almar@almarklein.org>
2021-07-18 23:51:16 +0200

    Proper implementation of events (#11)
    * implement events properly
    
    * change Python api wrt events
    
    * update examples
    
    * add vispy example
    
    * fmt
    
    * docstring for events
    
    * fmt

--- js/lib/widget.js
+++ js/lib/widget.js
@@ -128,76 +128,137 @@ var RemoteFrameBufferView = widgets.DOMWidgetView.extend({
         var that = this;
 
         // Create image element
-        this.img = new Image(1, 1);
+        this.img = new Image();
+        // Tweak loading behavior. These should be the defaults, but we set them just in case. 
         this.img.decoding = "sync";
         this.img.loading = "eager";
+        // Tweak mouse/touch/pointer/key behavior
+        this.img.style.touchAction = "none";  // prevent default pan/zoom behavior
+        this.img.ondragstart = () => false;  // prevent browser's built-in image drag
+        this.img.tabIndex = -1;
+        // Prevent context menu on RMB. Firefox still shows it when shift is pressed. It seems
+        // impossible to override this (tips welcome!), so let's make this the actual behavior.
+        this.img.oncontextmenu = function(e) { if (!e.shiftKey) { e.preventDefault(); e.stopPropagation(); return false; }};
+        
+        // Initialize the image
         this.img.src = this.model.last_frame.src;
         this.el.appendChild(this.img);
-        
         this.model.resolve_img_elements();
 
         // Set of throttler functions to send events at a friendly pace
         this._throttlers = {};
 
         // Initialize sizing. Setting the this.el's size right now has no effect for some reason, so we use a timer.
-        this.on_resizable();
         this.img.style.width = "100%";
         this.img.style.height = "100%";
+        this.el.style.resize = this.model.get('resizable') ? 'both' : 'none';
         window.setTimeout(() => { this.el.style.width = this.model.get('css_width'); }, 1);
         window.setTimeout(() => { this.el.style.height = this.model.get('css_height'); }, 1);
 
         // Keep track of size changes from the server
         this.model.on('change:css_width', function () { this.el.style.width = this.model.get('css_width'); }, this);
         this.model.on('change:css_height', function () { this.el.style.height = this.model.get('css_height'); }, this);
-        this.model.on('change:resizable', this.on_resizable, this);
-
+        this.model.on('change:resizable', function () { this.el.style.resize = this.model.get('resizable') ? 'both' : 'none'; }, this);
+        
         // Keep track of size changes in JS, so we can notify the server
         this._current_size = [0, 0, 1];
-        this._resizeObserver = new ResizeObserver(this.check_resize.bind(that));
+        this._resizeObserver = new ResizeObserver(this._check_resize.bind(that));
         this._resizeObserver.observe(this.img);
-        window.addEventListener("resize", this.check_resize.bind(this));       
+        window.addEventListener("resize", this._check_resize.bind(this));       
         
-        // Prevent context menu on RMB. Firefox still shows it when shift is pressed. It seems
-        // impossible to override this, so let's make this the actual behavior.
-        this.img.oncontextmenu = function(e) { if (!e.shiftKey) { e.preventDefault(); e.stopPropagation(); return false; }};
-        
-        // Mouse events
-        this.img.addEventListener('mousedown', function (e) {
-            let event = create_pointer_event(that.img, e, "mouse_down");
+        // Pointer events
+        this._pointers = {};
+        this.img.addEventListener('pointerdown', function (e) {
+            // This is what makes the JS PointerEvent so great. We can enable mouse capturing
+            // and we will receive mouse-move and mouse-up even when the pointer moves outside
+            // the element. Best of all, the capturing is disabled automatically! 
+            that.img.setPointerCapture(e.pointerId);
+            that._pointers[e.pointerId] = e;
+            let event = create_pointer_event(that.img, e, that._pointers, "pointer_down");
             that.send(event);
             if (!e.altKey) { e.preventDefault(); }
         });
-        this.img.addEventListener('mouseup', function (e) {
-            let event = create_pointer_event(that.img, e, "mouse_up");
+        this.img.addEventListener('lostpointercapture', function (e) {
+            // This happens on pointer-up or pointer-cancel. We threat them the same.
+            // The event we emit will still include the touch hat goes up.
+            let event = create_pointer_event(that.img, e, that._pointers, "pointer_up");
+            delete that._pointers[e.pointerId];
+            that.send(event);
+        });
+        this.img.addEventListener('pointermove', function (e) {
+            // If this pointer is not down, but other pointers are, don't emit an event.
+            if (that._pointers[e.pointerId] === undefined) {
+                if (Object.keys(that._pointers).length > 0) { return; }
+            }
+            let event = create_pointer_event(that.img, e, that._pointers, "pointer_move");
+            that.send_throttled(event, 50);
+        });
+
+        // Click events are not pointer events. Not sure if we need click events. It seems to make
+        // less sense, because the img is just a single element. Only double-click for now.
+        this.img.addEventListener('dblclick', function (e) {            
+            let event = create_pointer_event(that.img, e, {}, "double_click");
+            delete event["touches"];
+            delete event["ntouches"];
             that.send(event);
             if (!e.altKey) { e.preventDefault(); }
         });
-        this.img.addEventListener('click', function (e) {
-            let event = create_pointer_event(that.img, e, "click");
+
+        // Scrolling. Need a special throttling that accumulates the deltas.
+        this._wheel_state = {dx: 0, dy: 0, e: null, pending: false};
+        function send_wheel_event() {
+            let e = that._wheel_state.e;            
+            let rect = that.img.getBoundingClientRect();            
+            let event = {
+                event_type: "wheel",
+                x: Number(e.clientX - rect.left),
+                y: Number(e.clientY - rect.top),
+                dx: that._wheel_state.dx,
+                dy: that._wheel_state.dy,
+                modifiers: get_modifiers(e),
+            };
+            that._wheel_state.dx = 0;
+            that._wheel_state.dy = 0;
+            that._wheel_state.pending = false;
             that.send(event);
+        }
+        this.img.addEventListener('wheel', function (e) {
+            that._wheel_state.dx += e.deltaX * [1, 16, 600][e.deltaMode];
+            that._wheel_state.dy += e.deltaY * [1, 16, 600][e.deltaMode];
+            if (!that._wheel_state.pending) {
+                that._wheel_state.pending = true;
+                that._wheel_state.e = e;
+                window.setTimeout(send_wheel_event, 100);
+            }
             if (!e.altKey) { e.preventDefault(); }
         });
-        // TODO: mouse move
-        // TODO: wheel event
-        this.img.addEventListener('touchstart', function (e) {
-            that.send(create_pointer_event(that.img, e, "touch_down"));
-        });
-        this.img.addEventListener('touchend', function (e) {
-            that.send(create_pointer_event(that.img, e, "touch_up"));
-        });
-        this.img.addEventListener('touchcancel', function (e) {
-            that.send(create_pointer_event(that.img, e, "touch_up"));
-        });
-        this.img.addEventListener('touchmove', function (e) {
-            that.send(create_pointer_event(that.img, e, "touch_move"));
-        });
+
+        // Key events - approach inspired from ipyevents
+        function key_event_handler(e) {
+            // Failsafe in case the element is deleted
+            if (that.el.offsetParent === null) { return disable_key_event(); }
+            let event = {
+                event_type: "key_" + e.type.slice(3),
+                key: KEYMAP[e.key] || e.key,
+                modifiers: get_modifiers(e),
+            };
+            if (!e.repeat) { that.send(event); }  // dont do the sticky key thing
+            e.stopPropagation();
+            e.preventDefault();
+        }
+        function enable_key_event() {
+                document.addEventListener('keydown', key_event_handler, true);
+                document.addEventListener('keyup', key_event_handler, true);
+        }
+        function disable_key_event() {
+            document.removeEventListener('keydown', key_event_handler, true);
+            document.removeEventListener('keyup', key_event_handler, true);
+        }
+        this.el.addEventListener('pointerenter', enable_key_event, true);
+        this.el.addEventListener('pointerout', disable_key_event, true);
     },
 
-    on_resizable: function () {
-        this.el.style.resize = this.model.get('resizable') ? 'both' : 'none';
-    },
-
-    check_resize: function() {
+    _check_resize: function() {
         // Called when the widget resizes. Width and height are in logical pixels.
         let w = this.img.clientWidth;
         let h = this.img.clientHeight;
@@ -205,21 +266,35 @@ var RemoteFrameBufferView = widgets.DOMWidgetView.extend({
         if (w == 0 && h == 0) { return; }
         if (this._current_size[0] != w || this._current_size[1] != h || this._current_size[2] != r) {
             this._current_size = [w, h, r];
-            this.send_throttled({event_type: 'resize', width: w, height: h, pixel_ratio: r}, 500);
+            this.send_throttled({event_type: 'resize', width: w, height: h, pixel_ratio: r}, 200);
         }
     },
-    
+        
     send_throttled: function(msg, wait) {
+        // Like .send(), but throttled
         let event_type = msg.event_type || "";        
         let func = this._throttlers[event_type];
         if (func === undefined) {
-            func = throttled(this.send, wait || 200);
+            func = throttled(this.send, wait || 100);
             this._throttlers[event_type] = func;
         }
         func.call(this, msg);
     },
 
 });
+
+
+var KEYMAP = {
+    "Ctrl": "Control",
+    "Del": "Delete",
+    "Esc": "Escape",
+};
+
+
+function get_modifiers(e) {    
+    let modifiers = ["Alt", "Shift", "Ctrl", "Meta"].filter((n) => e[n.toLowerCase() + 'Key']);
+    return modifiers.map((m) => KEYMAP[m] || m);
+}
 
 
 function throttled(func, wait) {
@@ -250,60 +325,41 @@ function throttled(func, wait) {
 }
 
 
-function create_pointer_event(el, e, event_type) {
+function create_pointer_event(el, e, pointers, event_type) {
     let rect = el.getBoundingClientRect();
     let offset = [rect.left, rect.top];
+    let main_x = Number(e.clientX - offset[0]);
+    let main_y = Number(e.clientY - offset[1]);
 
-    if (event_type.startsWith("touch")) {
-        // Touch event - select one touch to represent the main position
-        let t = e.changedTouches[0];
-        var pos = [Number(t.clientX - offset[0]), Number(t.clientY - offset[1])];
-        var page_pos = [t.pageX, t.pageY];
-        var button = 0;
-        var buttons = [];
-        // Include basic support for multi-touch
-        var touches = {};
-        for (let i=0; i<e.changedTouches.length; i++) {
-            let t = e.changedTouches[i];
-            if (t.target !== e.target) { continue; }
-            touches[t.identifier] = [Number(t.clientX - offset[0]), Number(t.clientY - offset[1]), t.force];
-        }
-        var ntouches = e.touches.length;
-    } else {
-        // Mouse event
-        var pos = [Number(e.clientX - offset[0]), Number(e.clientY - offset[1])];
-        var page_pos = [e.pageX, e.pageY];
-        // Fix buttons
-        var buttons_mask = [];
-        if (e.buttons > 0) {
-            buttons_mask = Array.from(e.buttons.toString(2)).reverse();
-        } else if (e.which) {
-            buttons_mask = [e.which.toString(2)]  // e.g. Safari (but also 1 for RMB)
-        } else {
-            // libjavascriptcoregtk-3.0-0  version 2.4.11-1 does not define e.buttons
-            buttons_mask = [e.button.toString(2)]
-        }
-        var buttons = [1, 2, 3, 4, 5].filter((i) => buttons_mask[i-1] == "1");
-        var button = {0: 1, 1: 3, 2: 2, 3: 4, 4: 5}[e.button];
-        var touches = {'-1': [pos[0], pos[1], 1]};  // key must not clash with real touches
-        var ntouches = buttons.length;
+    // Collect touches (we may add more fields later)
+    let touches = {};
+    let ntouches = 0;
+    for (let pointer_id in pointers) {
+        let pe = pointers[pointer_id];   // pointer event
+        let x = Number(pe.clientX - offset[0]);
+        let y = Number(pe.clientY - offset[1]);
+        let touch = {x: x, y: y, pressure: pe.pressure};
+        touches[pe.pointerId] = touch;
+        ntouches += 1;
     }
 
-    // note: our button has a value as in JS "which"
-    modifiers = ["Alt", "Shift", "Ctrl", "Meta"].filter((n) => e[n.toLowerCase() + 'Key']);
-
-    // Create event dict
+    // Get button that changed, and the button state
+    var button = {0: 1, 1: 3, 2: 2, 3: 4, 4: 5, 5: 6}[e.button] || 0;
+    var buttons = [];
+    for (let b of [0, 1, 2, 3, 4, 5]) { if ((1 << b) & e.buttons) { buttons.push(b + 1); } }
+    
     return {
         event_type: event_type,
-        pos: pos,
-        page_pos: page_pos,
-        touches: touches,
-        ntouches: ntouches,
+        x: main_x,
+        y: main_y,
         button: button,
         buttons: buttons,
-        modifiers: modifiers,
+        modifiers: get_modifiers(e),
+        ntouches: ntouches,
+        touches: touches,
     }
 }
+
 
 module.exports = {
     RemoteFrameBufferModel: RemoteFrameBufferModel,
