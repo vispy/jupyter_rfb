944a34574a0e42943205348b3b8756acbf3a6cf8
Almar Klein <almar@almarklein.org>
2021-07-23 15:48:03 +0200

    update tests

--- tests/test_framesender.py
+++ tests/test_framesender.py
@@ -8,7 +8,8 @@ from jupyter_rfb.widget import FrameSenderMixin
 class MyFrameSender(FrameSenderMixin):
 
     max_buffered_frames = 1
-    max_queued_frames = 1
+
+    _rfb_draw_requested = False
 
     def __init__(self):
         super().__init__()
@@ -18,122 +19,148 @@ class MyFrameSender(FrameSenderMixin):
     def send(self, msg):
         self.msgs.append(msg)
 
+    def get_frame(self):
+        return np.array([[1, 2], [3, 4]], np.uint8)
 
-def test_framesender_1_1():
+    def trigger(self, request):
+        if request:
+            self._rfb_draw_requested = True
+        self._rfb_maybe_draw()
+
+
+def test_framesender_1():
 
     fs = MyFrameSender()
     fs.max_buffered_frames = 1
-    fs.max_queued_frames = 1
-
-    im = np.array([[1, 2], [3, 4]], np.uint8)
 
     assert len(fs.msgs) == 0
 
-    # Send a frame
-    fs.send_frame(im)
+    # Request a draw
+    fs.trigger(True)
     assert len(fs.msgs) == 1
-    assert len(fs._pending_frames) == 0
 
-    # Send another, this one is queued
-    fs.send_frame(im)
+    # Request another, no draw yet, because previous one is not yet confirmed
+    fs.trigger(True)
+    fs.trigger(True)
+    fs.trigger(True)
     assert len(fs.msgs) == 1
-    assert len(fs._pending_frames) == 1
 
-    # Queue is full, so this one is dropped
-    fs.send_frame(im)
-    assert len(fs.msgs) == 1
-    assert len(fs._pending_frames) == 1
-
-    assert fs.stats["received_frames"] == 3
     assert fs.stats["sent_frames"] == 1
-    assert fs.stats["dropped_frames"] == 1
     assert fs.stats["confirmed_frames"] == 0
 
     # Flush
     fs.frame_feedback["index"] = 1
     fs.frame_feedback["timestamp"] = fs.msgs[-1]["timestamp"]
     fs.frame_feedback["localtime"] = time.time()
-    fs._iter()
 
+    # Trigger, the previous request is still open
+    fs.trigger(False)
     assert len(fs.msgs) == 2
-    assert len(fs._pending_frames) == 0
 
-    # Now we can send another
-    fs.send_frame(im)
-    assert len(fs.msgs) == 2
-    assert len(fs._pending_frames) == 1
-
-    assert fs.stats["received_frames"] == 4
     assert fs.stats["sent_frames"] == 2
-    assert fs.stats["dropped_frames"] == 1
     assert fs.stats["confirmed_frames"] == 1
-
-
-def test_framesender_2_3():
-
-    fs = MyFrameSender()
-    fs.max_buffered_frames = 2
-    fs.max_queued_frames = 3
-
-    im = np.array([[1, 2], [3, 4]], np.uint8)
-
-    assert len(fs.msgs) == 0
-
-    # 1 Send a frame
-    fs.send_frame(im)
-    assert len(fs.msgs) == 1
-    assert len(fs._pending_frames) == 0
-
-    # 2 Send a frame
-    fs.send_frame(im)
-    assert len(fs.msgs) == 2
-    assert len(fs._pending_frames) == 0
-
-    # 1 Send another, this one is queued
-    fs.send_frame(im)
-    assert len(fs.msgs) == 2
-    assert len(fs._pending_frames) == 1
-
-    # 2 Send another, this one is queued
-    fs.send_frame(im)
-    assert len(fs.msgs) == 2
-    assert len(fs._pending_frames) == 2
-
-    # 3 Send another, this one is queued
-    fs.send_frame(im)
-    assert len(fs.msgs) == 2
-    assert len(fs._pending_frames) == 3
-
-    # Queue is full, so this one is dropped
-    fs.send_frame(im)
-    assert len(fs.msgs) == 2
-    assert len(fs._pending_frames) == 3
-
-    assert fs.stats["received_frames"] == 6
-    assert fs.stats["sent_frames"] == 2
-    assert fs.stats["dropped_frames"] == 1
-    assert fs.stats["confirmed_frames"] == 0
 
     # Flush
     fs.frame_feedback["index"] = 2
     fs.frame_feedback["timestamp"] = fs.msgs[-1]["timestamp"]
     fs.frame_feedback["localtime"] = time.time()
-    fs._iter()
 
-    assert len(fs.msgs) == 4
-    assert len(fs._pending_frames) == 1
+    fs.trigger(False)
+    assert len(fs.msgs) == 2
 
-    # Now we can send another
-    fs.send_frame(im)
-    assert len(fs.msgs) == 4
-    assert len(fs._pending_frames) == 2
+    assert fs.stats["sent_frames"] == 2
+    assert fs.stats["confirmed_frames"] == 2
 
-    assert fs.stats["received_frames"] == 7
-    assert fs.stats["sent_frames"] == 4
-    assert fs.stats["dropped_frames"] == 1
+    # Trigger with no request do not trigger a draw
+    # We *can* draw but *should* not.
+    fs.trigger(False)
+    assert len(fs.msgs) == 2
+
+    assert fs.stats["sent_frames"] == 2
+    assert fs.stats["confirmed_frames"] == 2
+
+    # One more draw
+    fs.trigger(True)
+    assert len(fs.msgs) == 3
+
+    assert fs.stats["sent_frames"] == 3
     assert fs.stats["confirmed_frames"] == 2
 
 
+def test_framesender_3():
+
+    fs = MyFrameSender()
+    fs.max_buffered_frames = 3
+
+    assert len(fs.msgs) == 0
+
+    # 1 Send a frame
+    fs.trigger(True)
+    assert len(fs.msgs) == 1
+
+    # 2 Send a frame
+    fs.trigger(True)
+    assert len(fs.msgs) == 2
+
+    # 3 Send a frame
+    fs.trigger(True)
+    assert len(fs.msgs) == 3
+
+    # 4 Send a frame - no draw, because first (3 frames ago) is no confirmed
+    fs.trigger(True)
+    assert len(fs.msgs) == 3
+
+    assert fs.stats["sent_frames"] == 3
+    assert fs.stats["confirmed_frames"] == 0
+
+    # Flush
+    fs.frame_feedback["index"] = 3
+    fs.frame_feedback["timestamp"] = fs.msgs[-1]["timestamp"]
+    fs.frame_feedback["localtime"] = time.time()
+
+    # Trigger with True. We request a new frame, but there was a request open
+    fs.trigger(True)
+    assert len(fs.msgs) == 4
+
+    assert fs.stats["sent_frames"] == 4
+    assert fs.stats["confirmed_frames"] == 3
+
+    # Flush
+    fs.frame_feedback["index"] = 4
+    fs.frame_feedback["timestamp"] = fs.msgs[-1]["timestamp"]
+    fs.frame_feedback["localtime"] = time.time()
+
+    # Trigger, but nothing to send (no frame pending)
+    fs.trigger(False)
+    assert len(fs.msgs) == 4
+
+    assert fs.stats["sent_frames"] == 4
+    assert fs.stats["confirmed_frames"] == 4
+
+    # We can but should not do a draw
+    fs.trigger(False)
+    assert len(fs.msgs) == 4
+
+    # Do three more draws
+    fs.trigger(True)
+    fs.trigger(True)
+    fs.trigger(True)
+    assert len(fs.msgs) == 7
+
+    # And request another (but this one will have to wait)
+    fs.trigger(True)
+    assert len(fs.msgs) == 7
+
+    # Flush
+    fs.frame_feedback["index"] = 7
+    fs.frame_feedback["timestamp"] = fs.msgs[-1]["timestamp"]
+    fs.frame_feedback["localtime"] = time.time()
+
+    # Trigger with False. no new request, but there was a request open
+    fs.trigger(False)
+    assert len(fs.msgs) == 8
+
+
 if __name__ == "__main__":
-    test_framesender_1_1()
-    test_framesender_2_3()
+    test_framesender_1()
+    test_framesender_3()
