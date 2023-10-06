cc639a56c2001b0bf7626d805edd5090a3b734aa
Kushal Kolar <kushalkolar@gmail.com>
2023-10-05 21:47:32 -0400

    Apply suggestions from code review
    Co-authored-by: Trevor Manz <trevor.j.manz@gmail.com>

--- jupyter_rfb/_colab.py
+++ jupyter_rfb/_colab.py
@@ -1,7 +1,6 @@
 try:
     # determine if running in colab
     from google.colab import output
-    from google.colab.output._widgets import _installed_url
 except Exception:
     IN_COLAB = False
     COLAB_URL = None
@@ -9,8 +8,6 @@ else:
     IN_COLAB = True
     # useful to enable widget manager here so the user doesn't have to manually
     output.enable_custom_widget_manager()
-    # the metadata that colab needs
-    COLAB_URL = _installed_url
 
 
 def get_colab_metadata():
@@ -22,7 +19,7 @@ def get_colab_metadata():
 
     if not IN_COLAB:
         return None
-
+      from google.colab.output._widgets import _installed_url as COLAB_URL
     meta = dict()
     meta["application/vnd.jupyter.widget-view+json"] = {
         "colab": {"custom_widget_manager": {"url": COLAB_URL}}
