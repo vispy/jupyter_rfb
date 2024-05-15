a2c9961c459354f87e288809a7e4b0708f09125a
Almar Klein <almar@almarklein.org>
2024-05-15 10:05:13 +0200

    Restore version of js module (#90)
    * Restore version of js module
    
    * fix ci and avoid yarn warnings
    
    * fix that local / ci need different dev deps

--- js/lib/index.js
+++ js/lib/index.js
@@ -1,4 +1,8 @@
 // Export widget models and views, and the npm package version number.
 
 export {RemoteFrameBufferModel, RemoteFrameBufferView} from './widget';
-export {version} from '../package.json';
+
+// export {version} from '../package.json'; -> yarn gives warning:
+// Should not import the named export 'version' (reexported as 'version') from default-exporting module (only default export is available soon)
+
+export const version = '0.1.0';  // we just keep the module-version at 0.1.0
