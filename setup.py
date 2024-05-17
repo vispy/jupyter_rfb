9a62359fa7731ad8c776a8e5bb52abac8b61e3f6
kushalkolar <kushalkolar@gmail.com>
2024-05-17 15:46:13 -0400

    cleanup old python version tags from setup.py

--- setup.py
+++ setup.py
@@ -81,8 +81,6 @@ setup_args = dict(
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Multimedia :: Graphics",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
